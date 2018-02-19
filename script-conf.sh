bcfa9c8f68e198aa259efee21f0d2b65e55576cd
J. Gonzalez <fjavi.gonzalez@gmail.com>
2018-02-19 16:16:33 +0100

    fix footer

--- script-conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SERVERNAME=/home/web/alleedubio.fr
-
-chown -R www-data.ftpgroup $SERVERNAME
-chmod -R 777 $SERVERNAME/log/spy.log
-chmod -R 777 $SERVERNAME/admin/FileUpload/server/php/files
