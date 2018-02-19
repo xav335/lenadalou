4b603d2affcd6c7494937a09eb8b3e9af9dbec23
J. Gonzalez <fjavi.gonzalez@gmail.com>
2018-02-19 17:29:23 +0100

    petits fix2

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 	<!-- Footer -->
 	<div class="row fullwidth footer">
 		<div class="row content">
-			<div class="large-3 medium-3 small-3 columns">
+			<div class="large-3 medium-3 small-0 columns pied" >
 				<a href="/">Accueil</a>
 				<a href="qsn.php">Le Nadalou</a>
 				<a href="activites.php">Sport et Nature</a>
@@ -11,13 +11,13 @@
 				<a href="contact.php">Contact</a>
 				<a href="mentions.php">mentions légales</a>
 			</div>
-			<div class="large-6 medium-6 small-6 columns">
+			<div class="large-6 medium-6 small-12 columns ">
 				<img src="img/logo-bottom.png" alt="" title="Le Nadalou" />
 				<p>Chemin du Malpas - 19400 MONCEAUX SUR DORDOGNE</p>
 				<p class="telephone">05 55 28 06 44 - 06 62 26 67 96</p>
-				<a href="http://www.iconeo.fr" target="_blank" style="font-size: 12px;">Powered by iconeo.fr</a>
+				<a class="pied" href="http://www.iconeo.fr" target="_blank" style="font-size: 12px;">Powered by iconeo.fr</a>
 			</div>
-			<div class="large-3 medium-3 small-3 columns">
+			<div class="large-3 medium-3 small-3 columns pied">
 				<img src="img/logo-ab.png" alt="" class="logo-ab" title="Gîtes de France" />
 			</div>
 			
